2a67a841f9057cc7d7e162bf9ba1492d03ca7e6a
Stanislav Muzulev <muz-stanislav@yandex.ru>
2025-03-17 23:36:21 +0300

    Реализация по ТЗ. Версия приложения 0.1

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,51 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
+@RequestMapping("/films")
 public class FilmController {
+    private final Map<Long, Film> films = new HashMap<>();
+    private long nextId = 1;
+    private static final Logger log = LoggerFactory.getLogger(FilmController.class);
+
+
+
+    @PostMapping
+    public Film addFilm(@Valid @RequestBody Film film) {
+        film.setId(nextId++); // Генерация нового ID
+        films.put(film.getId(), film);
+        log.info("Добавлен фильм: {}", film);
+        return film;  // Возвращаем просто объект Film
+    }
+
+    @PutMapping
+    public Film updateFilm(@Valid @RequestBody Film film) {
+        if (!films.containsKey(film.getId())) {
+            throw new NotFoundException("Фильм с id " + film.getId() + " не найден");
+        }
+        films.put(film.getId(), film);
+        log.info("Обновлен фильм: {}", film);
+        return film;  // Возвращаем просто объект Film
+    }
+
+    @GetMapping
+    public Collection<Film> getAllFilms() {
+        return films.values();
+    }
 }
---
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ErrorHandler.java
@@ -0,0 +1,38 @@
+package ru.yandex.practicum.filmorate.exception;
+
+import jakarta.validation.ValidationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class ErrorHandler {
+
+    @ExceptionHandler(NotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Map<String, String> handleNotFound(final NotFoundException e) {
+        return Map.of("error", "Не найдено", "message", e.getMessage());
+    }
+
+    @ExceptionHandler(ValidationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleValidation(final ValidationException e) {
+        return Map.of("error", "Ошибка валидации", "message", e.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage()));
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+}
