f9f3c9eb10ee383715aa7a8e55cd662765a6aef9
Stanislav Muzulev <muz-stanislav@yandex.ru>
2025-03-28 01:07:48 +0300

    Реализована логика добавления дружбы, лайков, тесты Postman пройдены

---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/InMemoryUserStorage.java
@@ -0,0 +1,39 @@
+package ru.yandex.practicum.filmorate.storage.user;
+
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.util.*;
+
+@Component
+public class InMemoryUserStorage implements UserStorage {
+    private final Map<Long, User> users = new HashMap<>();
+    private long nextId = 1;
+
+    @Override
+    public User add(User user) {
+        user.setId(nextId++);
+        users.put(user.getId(), user);
+        return user;
+    }
+
+    @Override
+    public User update(User user) {
+        if (!users.containsKey(user.getId())) {
+            throw new NotFoundException("Пользователь с ID " + user.getId() + " не найден");
+        }
+        users.put(user.getId(), user);
+        return user;
+    }
+
+    @Override
+    public Optional<User> getById(Long id) {
+        return Optional.ofNullable(users.get(id));
+    }
+
+    @Override
+    public List<User> findAll() {
+        return new ArrayList<>(users.values());
+    }
+}
